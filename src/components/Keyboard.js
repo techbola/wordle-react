a1ea93b44e4b79ca78a9f17640b4b91866f9b585
Adebola <bfagbowore@gmail.com>
2022-08-25 06:54:41 +0100

    abstract code to select letter, enter, and delete letter from board to app.js file

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -10,18 +10,18 @@ function Keyboard() {
     <div className="keyboard">
       <div className="line1">
         {keys1.map((key) => {
-          return <Key keyVal={key} />;
+          return <Key key={key} keyVal={key} />;
         })}
       </div>
       <div className="line2">
         {keys2.map((key) => {
-          return <Key keyVal={key} />;
+          return <Key key={key} keyVal={key} />;
         })}
       </div>
       <div className="line3">
         <Key keyVal={"ENTER"} bigKey />
         {keys3.map((key) => {
-          return <Key keyVal={key} />;
+          return <Key key={key} keyVal={key} />;
         })}
         <Key keyVal={"DELETE"} bigKey />
       </div>
