0e8a779d7b1d81c86dac5d2fdfcb3e527cb825f5
Adebola <bfagbowore@gmail.com>
2022-08-25 06:29:13 +0100

    keyboard design, listen to keyboard click event

---
+++ src/components/Key.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+
+function Key({ keyVal, bigKey }) {
+  const { board, setBoard } = useContext(AppContext);
+
+  const selectLetter = () => {
+    const newBoard = [...board];
+    newBoard[0][0] = keyVal;
+    setBoard(newBoard);
+  };
+
+  return (
+    <div className="key" id={bigKey && "big"} onClick={selectLetter}>
+      {keyVal}
+    </div>
+  );
+}
+
+export default Key;
